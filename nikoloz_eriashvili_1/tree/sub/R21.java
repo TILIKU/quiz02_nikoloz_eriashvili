30980e23b52bb542af84493ed4b8326590ed40f3
TILIKU <85792875+TILIKU@users.noreply.github.com>
2024-03-25 12:09:49 +0400

    Add files via upload

---
+++ nikoloz_eriashvili_1/tree/sub/R21.java
@@ -0,0 +1,16 @@
+package sub;
+
+import A22;
+import tree.A21;
+
+public class R21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A21 a21 = new A21();
+		A22 a22 = new A22();
+		System.out.println("area = " + (A21.hei * A22.len) );
+		System.out.println("name of a month= " + A22.month);
+	}
+
+}
